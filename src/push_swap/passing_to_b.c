b480b135f85c81da61bbf4702a0d339fc276dc6f
student-404-error <ksh30102@gmail.com>
2024-06-17 18:45:30 +0200

    Make 3 range

--- src/push_swap/passing_to_b.c
+++ src/push_swap/passing_to_b.c
@@ -6,7 +6,7 @@
 /*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 16:28:43 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/06/17 17:40:51 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/06/17 18:43:39 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,24 @@ void	passing_to_b(t_list *a, t_list *b)
 	t_stack	*a_stk;
 	t_stack	*b_stk;
 	int		size;
+	int		cnt = 3;
 
 	a_stk = ft_new_stack(a);
 	b_stk = ft_new_stack(b);
 	size = ft_lstsize(a);
 	int	i = size / 3;
-	while (i < size)
+	while (cnt)
 	{
+		ft_printf("i = %d\n", i);
 		while (a_stk->min <= i)
 		{
-			status_list_and_stack(a, a_stk, "a");
 			if (a->idx <= i)
 				move_push(&b, &a, &b_stk, &a_stk);
 			else
 				move_rotate(&a, &a_stk);
 		}
-		i += size / 3;
+		i += size / 3 + 1;
+		cnt--;
 	}
 	status_list_and_stack(a, a_stk, "a");
 	status_list_and_stack(b, b_stk, "b");
