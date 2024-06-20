a3cfda1eca0a8da74d5807cdb6a719f7d3900fcc
student-404-error <ksh30102@gmail.com>
2024-06-20 18:19:06 +0200

    Update Three argu

--- src/push_swap/three_argu.c
+++ src/push_swap/three_argu.c
@@ -6,7 +6,7 @@
 /*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 18:22:53 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/06/11 18:58:13 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/06/20 18:08:58 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,29 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	three_argu(t_list **a_list)
+void	three_argu(t_list **a_list, t_stack **a_stk)
 {
-	t_list	*first;
-	t_list	*second;
-	t_list	*third;
+	t_list	*f;
+	t_list	*s;
+	t_list	*t;
 
-	first = *a_list;
-	second = first->next;
-	third = second->next;
-	// 1 2 3 > 0
-	// 1 3 2 > sa ra
-	// 2 1 3 > sa
-	// 2 3 1 > rra
-	// 3 1 2 > ra
-	// 3 2 1 > sa rra
-	if (first->content > second->content && second->content > third->content)
+	f = *a_list;
+	s = f->next;
+	t = s->next;
+	if (f->content > s->content && s->content > t->content)
 	{
-	//	move_swap(a_list);
-	//	move_reverse_rotate(a_list);
-		ft_printf("sa\nrra\n");
+		move_swap(a_list, a_stk, "sa");
+		move_reverse_rotate(a_list, a_stk, "rra");
 	}
-	else if (first->content > second->content && second->content < third->content)
+	else if (f->content > s->content && f->content < t->content)
+		move_swap(a_list, a_stk, "sa");
+	else if (f->content > s->content && f->content > t->content)
+		move_rotate(a_list, a_stk, "ra");
+	else if (f->content < s->content && t->content < f->content)
+		move_reverse_rotate(a_list, a_stk, "rra");
+	else if (f->content < t->content && s->content > t->content)
 	{
-	//	move_rotate(a_list);
-		ft_printf("ra\n");
-	}
-	else if (first->content < second->content && third->content < first->content)
-	{
-	//	move_reverse_rotate(a_list);
-		ft_printf("rra\n");
-	}
-	else if (first->content > second->content && first->content < third->content)
-	{
-	//	move_swap(a_list);
-		ft_printf("sa\n");
-	}
-	else if (first->content < third->content && second->content > third->content)
-	{
-	//	move_swap(a_list);
-	//	move_rotate(a_list);
-		ft_printf("sa\nra\n");
+		move_swap(a_list, a_stk, "sa");
+		move_rotate(a_list, a_stk, "ra");
 	}
 }
