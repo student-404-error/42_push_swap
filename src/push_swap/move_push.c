74fe557a3c4ea537e43473cc2800f49fe436eceb
student-404-error <ksh30102@gmail.com>
2024-06-21 22:15:15 +0200

    Change content -> idx

--- src/push_swap/move_push.c
+++ src/push_swap/move_push.c
@@ -6,7 +6,7 @@
 /*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 19:01:30 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/06/20 17:36:17 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/06/21 21:17:54 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int	update_stack(t_list *origin, int flag)
 		return (INT_MAX);
 	if (origin == NULL && flag == 1)
 		return (INT_MIN);
-	comp = origin->content * flag;
+	comp = origin->idx * flag;
 	while (origin)
 	{
-		if (comp < origin->content * flag)
-			comp = origin->content * flag;
+		if (comp < origin->idx * flag)
+			comp = origin->idx * flag;
 		origin = origin->next;
 	}
 	return (comp * flag);
@@ -46,14 +46,14 @@ void	move_push(t_list **to_lst, t_list **fr_lst, t_stack **to, t_stack **fr)
 		(*fr)->bottom = NULL;
 	node->next = NULL;
 	ft_lstadd_front(to_lst, node);
-	if (node->content == (*fr)->min)
+	if (node->idx == (*fr)->min)
 		(*fr)->min = update_stack(*fr_lst, -1);
-	if (node->content == (*fr)->max)
+	if (node->idx == (*fr)->max)
 		(*fr)->max = update_stack(*fr_lst, 1);
-	if ((*to)->min > node->content)
-		(*to)->min = node->content;
-	if ((*to)->max < node->content)
-		(*to)->max = node->content;
+	if ((*to)->min > node->idx)
+		(*to)->min = node->idx;
+	if ((*to)->max < node->idx)
+		(*to)->max = node->idx;
 	(*fr)->top = *fr_lst;
 	(*to)->top = *to_lst;
 	(*to)->bottom = ft_lstlast(*to_lst);
