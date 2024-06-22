261b9f6dd4100cf40a21fd961127b659ae83ae51
student-404-error <127807924+student-404-error@users.noreply.github.com>
2024-06-22 20:02:27 +0200

    Create bonus header

---
+++ include/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/23 17:12:14 by seong-ki          #+#    #+#             */
+/*   Updated: 2024/06/22 18:03:12 by seong-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+char	*get_next_line(int fd);
+char	*gnl_join(char *s1, char *s2);
+#endif
---
+++ include/push_swap_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_bonus.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seong-ki <seong-ki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/22 19:09:36 by seong-ki          #+#    #+#             */
+/*   Updated: 2024/06/22 19:54:14 by seong-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_BONUS_H
+# define PUSH_SWAP_BONUS_H
+
+void	checker(t_list **a, t_list **b);
+void	push(t_list **to_lst, t_list **fr_lst);
+void	rotate(t_list **lst);
+void	reverse_rotate(t_list **lst);
+void	rr(t_list **a, t_list **b);
+void	rrr(t_list **a, t_list **b);
+void	swap(t_list **lst);
+void	ss(t_list **a, t_list **b);
+void	movement_swap(t_list **a, t_list **b, char *option);
+void	movement_rotate(t_list **a, t_list **b, char *option);
+void	movement_reverse_rotate(t_list **a, t_list **b, char *option);
+void	movement_push(t_list **a, t_list **b, char *option);
+
+#endif // !PUSH_SWAP_BONUS_H
